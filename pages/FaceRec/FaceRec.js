0942d6e484b1f3a186689cbec39b4db765491a78
Jyc <Jyc18917899539@163.com>
2019-09-17 08:02:12 +0800

    Initial Commit

---
+++ pages/FaceRec/FaceRec.js
@@ -0,0 +1,85 @@
+// pages/FaceRec/FaceRec.js
+var arr=[];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  upLoad:function(){
+    data:{
+      src:''
+    }
+
+    var self=this;
+    wx.chooseImage({
+      count:1,
+      sizeType:['original','compressed'],
+      sourceType:['album','camera'],
+      success: function(res) {
+          var tempFilePaths = res.tempFilePaths;
+          arr=arr.concat(tempFilePaths);
+          self.setData({src:arr});
+      }
+    })
+  }
+})
