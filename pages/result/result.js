62dd1691254a3049cfe26deeb37a200d4a92c7c8
Jyc <Jyc18917899539@163.com>
2019-10-10 22:27:16 +0800

    结果页面demo

---
+++ pages/result/result.js
@@ -0,0 +1,76 @@
+// pages/result/result.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    res:[{
+      name:"1",
+      imgurl: "https://i.loli.net/2019/10/10/x1L3UlFcCQ5yZBp.jpg"
+    },
+    {
+      name:"2",
+      imgurl:"https://i.loli.net/2019/10/10/wdg5HNcC6AQzlnB.jpg"
+    } 
+
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
